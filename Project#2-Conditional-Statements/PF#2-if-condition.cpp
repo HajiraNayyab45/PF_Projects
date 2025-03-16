041a1438d1daa5ca3a4d858124a7a20a75a64504
Hajira Nayyab <hajra.bsse4515@iiu.edu.pk>
2025-03-16 13:11:16 +0500

    Add files via upload

---
+++ Project#2-Conditional-Statements/PF#2-if-condition.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num;
+
+    // Ask the user to enter a number
+    cout << "Enter a number: ";
+    cin >> num;
+
+    // If condition to check if the number is positive
+    if (num > 0) {
+        cout << "The number is positive." << endl;
+    }
+
+    return 0;
+}
